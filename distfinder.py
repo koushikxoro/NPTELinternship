eabbd00ffb34f2c945268bc0d2968bd3c84b4235
koushikxoro <40536408+koushikxoro@users.noreply.github.com>
2018-12-29 18:09:25 +0530

    improved over the previous

--- distfinder.py
+++ distfinder.py
@@ -5,15 +5,39 @@ import string
 import networkx as nx
 
 
-filename="dict.txt"
-filename1="distance.txt"
+
 prime_dict={}
-file1=open(filename,"r")
-file2=open(filename1,"w")
-for line in file1:
+file1=open("dict.txt","r")
+file2=open("distance.txt","w")
+file3=open("graph_modified.txt","r")
+
+g=nx.Graph()
+
+for line in file3:
     words=line.split()
-    prime_dict[words[0]]=words[1]
-g=nx.read_gml('graph_main')
+    l1=[]
+    for ele in words:
+        int_ele=int(ele)
+        l1.append(int_ele)
+    t1=tuple(l1)
+    l2=[t1]
+    g.add_weighted_edges_from(l2)
+print("Graph making complete")
+
+
+
+count_main=0
+count=0
+for line in file1:
+    count_main+=1
+    words=line.split()
+    prime_dict[words[0]]=int(words[1])
+    
+
+print("dictionary complete")
+print(prime_dict.keys())
+
+
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
@@ -23,8 +47,7 @@ def hasPunctuations(inputString):
         return True
     else:
         return False
-x=[]
-y=[]
+
 def distme(path):
      tree = ET.parse(path)
      root = tree.getroot()
@@ -32,9 +55,11 @@ def distme(path):
      
      count_rev=0
     
+     print("In function")
      for rev in root.find('{http://www.mediawiki.org/xml/export-0.10/}page').findall('{http://www.mediawiki.org/xml/export-0.10/}revision'):
          
          count_rev+=1
+         print(count_rev)
          file2.write("\n-----Revision----- "+str(count_rev)+"\n")
         
          text = rev.find('{http://www.mediawiki.org/xml/export-0.10/}text').text;
@@ -46,12 +71,12 @@ def distme(path):
          tagged_sent =  pos_tag(word_tokenize(text))
          
          m=0
-         all_count=0
-         real_count=0
-         while True:
 
+         while True:
+             print("we")
              if hasNumbers(tagged_sent[m][0]) == False and hasPunctuations(tagged_sent[m][0]) == False and len(tagged_sent[m][0]) > 1 and tagged_sent[m][1] in tags:
                  first=tagged_sent[m][0]
+                 
                  break
              m+=1    
          
@@ -61,15 +86,24 @@ def distme(path):
              if hasNumbers(tagged_word[0]) == False and hasPunctuations(tagged_word[0]) == False and len(tagged_word[0]) > 1:  #to remove words like ",","132" etc.
                  if(tagged_word[1] in tags):
                     
-                     if tagged_word[0] in prime_dict.keys():
-                         a1=prime_dict[first]   
+                     print("Hello")
+                     if tagged_word[0] in prime_dict.keys() and first in prime_dict.keys():
+                         print(".")
+                         a1=prime_dict[first]
                          a2=prime_dict[tagged_word[0]]
-                         l1=nx.shortest_path_length(g,source=int(a1),target=int(a2))
-                         first=tagged_word[0]
-                         file2.write(str(l1))
+                         a1=int(a1)
+                         a2=int(a2)
+                         if a1 in g and a2 in g:
+                             if nx.has_path(g,a1,a2):
+                                 
+                                 l1=nx.shortest_path_length(g,source=a1,target=a2)
+                                 
+                                 file2.write(str(l1))
+                     first=tagged_word[0]
+                     
+                    
 
 distme("Brockway_Mountain_Drive.xml")
-                         
-                    
-         
-         
+file1.close()
+file2.close()
+file3.close()
