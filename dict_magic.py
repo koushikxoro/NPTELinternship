88fdfac24b42b50b941addb8bd1dadeee550c73c
koushikxoro <40536408+koushikxoro@users.noreply.github.com>
2019-01-10 22:52:22 +0530

    @descentis i have pushed the code

---
+++ dict_magic.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 22:17:12 2019
+
+@author: computerlab
+"""
+import xml.etree.cElementTree as ET
+import mwparserfromhell
+def magic(path):
+     tree = ET.parse(path)
+     root = tree.getroot()
+     root = tree.getroot()
+     
+     count_rev=0
+     mdic={}
+     print("In function")
+     for rev in root.find('{http://www.mediawiki.org/xml/export-0.10/}page').findall('{http://www.mediawiki.org/xml/export-0.10/}revision'):
+         
+         count_rev+=1
+         print(count_rev)
+         
+         
+        
+         text = rev.find('{http://www.mediawiki.org/xml/export-0.10/}text').text;
+         if(not text):
+             
+             continue
+         wikicode=mwparserfromhell.parse(text)
+         templates=wikicode.filter_wikilinks()
+         if(len(templates)==0):
+             continue
+         first=templates[0]
+         for i in templates[1:]:
+             str1=str(first)
+             str2=str(i)
+             str3=str1[2:len(str1)-2]+"$#$$@*"+str2[2:len(str2)-2]
+             if str3 not in mdic.keys():
+                 mdic[str3]=1
+             first=i
+         
+         #print(templates)
+     #print(mdic)
+             
+                     
+                    
+
+magic("Zhang_Heng.xml")
